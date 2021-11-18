4dad10b6721697f17b642d7c16889faebd8beb8b
ajia <ajialeeholiday@gmail.com>
2021-11-18 11:31:03 -0700

    fixed server connection

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const { note } = require('./db/db.json');
+const fs = require('fs');
+const note = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 
 app.get('/notes', (req, res) => {
     res.json(note);
 });
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
 });
