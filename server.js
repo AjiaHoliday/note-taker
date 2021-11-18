77620f2464d306676f53d3ed6f175a2eaa58b85d
ajia <ajialeeholiday@gmail.com>
2021-11-18 13:21:24 -0700

    create note function

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const notes = require('./db/db.json');
+const note = require('./db/db.json');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 //Create a new note function
@@ -12,18 +13,25 @@ function createNote(body, note) {
         path.join(__dirname, './db/db.json'),
         JSON.stringify(note, null, 2)
     );
-    
 };
 
 app.get('/notes', (req, res) => {
-    res.json(notes);
+    res.json(noten);
 });
 
 app.post('/notes', (req, res) =>{
     req.body.id = note.length.toString();
     const notes = createNote(req.body, note);
     res.json(notes);
-})
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+
+app.get('/notes', (req, res)=>{
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
